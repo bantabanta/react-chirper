c8f96ec916726bb39457791ad343832f0460523a
Tim Banta <timbanta@me.com>
2022-11-03 14:26:01 -0700

    feat: add tweet UI to dashboard
    includes install of react-icons for UI

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,11 +1,63 @@
 import { connect } from "react-redux";
-import { formatTweet } from "../utils/helpers";
+import { formatDate, formatTweet } from "../utils/helpers";
+import {
+  TiArrowBackOutline,
+  TiHeartOutline,
+  TiHeartFullOutline,
+} from "react-icons/ti";
 
 const Tweet = (props) => {
+  const toParent = (e, id) => {
+    e.preventDefault();
+
+    // TODO: Build redirect to parent tweet
+  };
+
+  const handleLike = (e) => {
+    e.preventDefault();
+
+    // TODO: Build handle liking the Tweet
+  };
+
   if (props.tweet === null) {
     return <p>This Tweet doesn't exist</p>;
   }
-  return <div className="tweet"></div>;
+
+  const { name, avatar, timestamp, text, hasLiked, likes, replies, parent } =
+    props.tweet;
+
+  return (
+    <div className="tweet">
+      <img src={avatar} alt={`Avatar of ${name}`} className="avatar" />
+      <div className="tweet-info">
+        <div>
+          <span>{name}</span>
+          <div>{formatDate(timestamp)}</div>
+          {parent && (
+            <button
+              className="replying-to"
+              onClick={(e) => toParent(e, parent.id)}
+            >
+              Replying to @{parent.author}
+            </button>
+          )}
+          <p>{text}</p>
+        </div>
+      </div>
+      <div className="tweet-icons">
+        <TiArrowBackOutline className="tweet-icon" />
+        <span>{replies !== 0 && replies}</span>
+        <button className="heart-button" onClick={handleLike}>
+          {hasLiked === true ? (
+            <TiHeartFullOutline color="e0245e" className="tweet-icon" />
+          ) : (
+            <TiHeartOutline className="tweet-icon" />
+          )}
+        </button>
+        <span>{likes !== 0 && likes}</span>
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = ({ authedUser, users, tweets }, { id }) => {
