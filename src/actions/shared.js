edf9d7b5bef94cad2d36c239e42178bc06f315fc
Tim Banta <timbanta@me.com>
2022-11-03 13:42:06 -0700

    feat: added UI to show tweet Id's

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,11 +3,23 @@ import { receiveUsers } from "./users";
 import { receiveTweets } from "./tweets";
 import { setAuthedUser } from "./authedUser";
 
-const AUTHED_ID = 'tylermcginnis';
+export const RECEIVE_DATA = "RECEIVE_DATA";
+
+// action creator
+
+function receiveData(todos, goals) {
+  return {
+    type: RECEIVE_DATA,
+  };
+}
+
+const AUTHED_ID = "tylermcginnis";
+
+// asynchronous action creator
 
 export function handleInitialData() {
   return (dispatch) => {
-    return getInitialData().then(({users, tweets}) => {
+    return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveUsers(users));
       dispatch(receiveTweets(tweets));
       dispatch(setAuthedUser(AUTHED_ID));
